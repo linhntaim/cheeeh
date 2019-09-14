643b5cea08bbdd36fced17e4b78d8ff4433d3072
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2019-09-14 13:24:28 +0700

    fix

--- resources/views/emails/user_password_reset/vi.blade.php
+++ resources/views/emails/user_password_reset/vi.blade.php
@@ -8,7 +8,7 @@
 <p>
     Xin chào {{ $x_email_to_name }},<br>
     <br>
-    Vui lòng bám vào đường dẫn sau để thiết lập lại mật khẩu của bạn:<br>
+    Vui lòng bấm vào đường dẫn sau để thiết lập lại mật khẩu của bạn:<br>
     <a href="{{ $url_reset_password }}">{{ $url_reset_password }}</a><br>
     <br>
     Nếu bạn không phải là người đưa ra yêu cầu thiết lập lại mật khẩu, hãy bỏ qua nội dung thư này.<br>
